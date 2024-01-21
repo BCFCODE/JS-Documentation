d9f353fe409afa9a3033d08a1711537a3cc919b3
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-01-21 12:06:42 +0330

    show/hide button menu functionality implemented and some responsive bugs fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-const d = new Date();
-const year = document.getElementById("year")
-year.textContent = d.getFullYear()
+document.addEventListener('DOMContentLoaded', () => {
+  showCopyRightYearDynamically()
+  showHideMenu()
+
+  // Add other main.js functionality here if needed
+});
+
+const showHideMenu = () => {
+  const menuBtn = document.getElementById("menuBtn");
+
+  menuBtn.textContent = 'Show Menu';
+  menuBtn.addEventListener('click', () => {
+    const navbar = document.querySelector('.navbar');
+    if (menuBtn.textContent === 'Show Menu') {
+      navbar.style.opacity = 1;
+      menuBtn.textContent = 'Hide Menu';
+    } else {
+      navbar.style.opacity = 0;
+      menuBtn.textContent = 'Show Menu';
+    }
+  });
+};
+
+const showCopyRightYearDynamically = () => {
+  const d = new Date();
+  const year = document.getElementById("year")
+  year.textContent = d.getFullYear()
+}
+
